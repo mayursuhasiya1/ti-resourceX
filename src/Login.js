b12e4629af8a6ae12d4fe05a271233b8460755e4
Mayur Suhasiya <mayur.suhasiya@technoidentity.com>
2022-09-22 10:49:31 +0530

    added name in left top corner

--- src/Login.js
+++ src/Login.js
@@ -10,46 +10,55 @@ const Login = () => {
 
   return (
 <div>
-        
-  <div className="logo">
+
+ <div className="main">
+
+	{/* left portion */}
+   <div className="left-part">
+	 <div className="left-top">
+	    <h1 className="heading">Ti-ResourceX</h1>
+	    
+	 </div>
+	 <span className="motto">Your one stop solution for employee management</span>
      <a href="https://technoidentity.com/"><img src="tiLogoNew.png" alt="Technoidentity"/></a>
+   </div>
+
+   {/* right portion */}
+     {/* sign in box */}
+  <div className="right-part">
+
+{/*box */}
+<div class="box">
+ <form autocomplete="off" >
+	 <h2>Sign in</h2>
+	 <div class="inputBox">
+ <input type="text" required="required" onChange={e => setUserName(e.target.value)}/>
+		 <span>Userame</span>
+		 <i></i>
+	 </div>
+	 <div class="inputBox">
+ <input type="password" required="required" onChange={e => setPassword(e.target.value)}/>
+
+		 <span>Password</span>
+		 <i></i>
+	 </div>
+	 <div class="links">
+		 <a href="#">Forgot Password ?</a>
+		 <a href="#">Signup</a>
+	 </div>
+	 <input type="submit" value="Login"/>
+	 
+	 <div className="google_logo"> 
+	   <a href="">Sign in with<img src="google.png" alt="" /></a>
+	 </div> 
+   </form>
+ 
+   </div>
+
   </div>
-    
-	{/* sign in box */}
-  <div className="signin-portion">
-
-       {/*box */}
-    <div class="box">
-		<form autocomplete="off" >
-			<h2>Sign in</h2>
-			<div class="inputBox">
-        <input type="text" required="required" onChange={e => setUserName(e.target.value)}/>
-				<span>Userame</span>
-				<i></i>
-			</div>
-			<div class="inputBox">
-        <input type="password" required="required" onChange={e => setPassword(e.target.value)}/>
-
-				<span>Password</span>
-				<i></i>
-			</div>
-			<div class="links">
-				<a href="#">Forgot Password ?</a>
-				<a href="#">Signup</a>
-			</div>
-			<input type="submit" value="Login"/>
-			
-			<div className="google_logo"> 
-			  <a href="">Sign in with<img src="google.png" alt="" /></a>
-			</div>
-			
-		</form>
-		
-	</div>
-
-  </div>
-  {/* signin portion end*/}
-
+{/* signin portion end*/}
+ </div>
+        
 </div>
   );
 }
