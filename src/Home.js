916133e5d5d0d4a4133c9634b4ca68bbf26e7166
Mayur Suhasiya <mayur.suhasiya@technoidentity.com>
2022-09-27 15:33:46 +0530

    Dummy homepage added

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useState } from 'react';
 
-import './Home.css';
+// import './Home.css';
 
 const Home = () => {
 
@@ -10,11 +10,35 @@ const Home = () => {
     const [password, setPassword] = useState();
 
   return (
-    <div >
-      
-       <h1>Home Page</h1>
+   
 
-    </div>
+    <div class="bg-grey-lighter min-h-screen flex flex-col">
+                <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+
+                    <h1 class="mb-8 text-3xl text-center">Home page</h1> 
+
+                    <a href="./login">
+                    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 ml-6 mr-24 hover:border-blue-500 rounded">
+                       Log in
+                    </button>
+                    </a>
+                    <a href="./signup">
+                      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                       Sign up
+                      </button>
+                    </a>
+                        
+    
+                       
+                    </div>
+    
+                   
+                </div>
+            </div>
+  
+         
+   
   )
 }
 
