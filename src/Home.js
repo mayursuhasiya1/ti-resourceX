05d10d789ce25674fe3fc4457fff3b0a3237bd89
Mayur Suhasiya <mayur.suhasiya@technoidentity.com>
2022-09-27 11:14:20 +0530

    change in logo text

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
  <div class="justify-center ">
     <div class="flex items-center justify-between h-12 text-white bg-gray-400 shadow-md sm:h-20">     
         <a href="https://technoidentity.com/"><img class="h-14 py-2 pr-4 ml-8 sm:h-24" src="tiLogoNew.png"></img></a>
-        <h1 className="heading ml-4 text-xl text-black font-bold ml- sm:text-4xl mr-24">TI-ResourceX</h1>
+        <h1 className="heading ml-4 text-lg text-black font-bold ml- sm:text-4xl mr-24">TI-ResourceX</h1>
         <div class="flex flex-col px-4">
 
             {/* <p class="text-2xl font-semibold uppercase">19 Oct 2021</p> */}
